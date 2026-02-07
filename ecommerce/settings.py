43f7aabf971cd07f5a1014e30c503b3c88c10c3f
TechBus-Soft <techbuspos@gmail.com>
2026-02-07 22:30:50 +0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -151,10 +151,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # this email will be used to send emails
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # host email password required
